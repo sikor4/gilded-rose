8359f4d9692349c3e83a9710aa47b13d910d9bfc
sikor4 <sikorskiemu@gmail.com>
2024-11-02 12:25:08 +0100

    Strict types && simplify

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -6,42 +6,42 @@ final class GildedRose
 {
     public function updateQuality($item)
     {
-        if ($item->name != ItemName::AGED_BRIE->value and $item->name != ItemName::BACKSTAGE_PASS->value) {
+        if ($item->name !== ItemName::AGED_BRIE->value and $item->name !== ItemName::BACKSTAGE_PASS->value) {
             if ($item->quality > 0) {
-                if ($item->name != ItemName::SULFURAS->value) {
-                    $item->quality = $item->quality - 1;
+                if ($item->name !== ItemName::SULFURAS->value) {
+                    $item->quality--;
                 } else {
                     $item->quality = 80;
                 }
             }
         } else {
             if ($item->quality < 50) {
-                $item->quality = $item->quality + 1;
-                if ($item->name == ItemName::BACKSTAGE_PASS->value) {
+                $item->quality++;
+                if ($item->name === ItemName::BACKSTAGE_PASS->value) {
                     if ($item->sell_in < 11) {
                         if ($item->quality < 50) {
-                            $item->quality = $item->quality + 1;
+                            $item->quality++;
                         }
                     }
                     if ($item->sell_in < 6) {
                         if ($item->quality < 50) {
-                            $item->quality = $item->quality + 1;
+                            $item->quality++;
                         }
                     }
                 }
             }
         }
 
-        if ($item->name != ItemName::SULFURAS->value) {
+        if ($item->name !== ItemName::SULFURAS->value) {
             $item->sell_in = $item->sell_in - 1;
         }
 
         if ($item->sell_in < 0) {
-            if ($item->name != ItemName::AGED_BRIE->value) {
-                if ($item->name != ItemName::BACKSTAGE_PASS->value) {
+            if ($item->name !== ItemName::AGED_BRIE->value) {
+                if ($item->name !== ItemName::BACKSTAGE_PASS->value) {
                     if ($item->quality > 0) {
-                        if ($item->name != ItemName::SULFURAS->value) {
-                            $item->quality = $item->quality - 1;
+                        if ($item->name !== ItemName::SULFURAS->value) {
+                            $item->quality--;
                         }
                     }
                 } else {
@@ -49,7 +49,7 @@ final class GildedRose
                 }
             } else {
                 if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
+                    $item->quality++;
                 }
             }
         }
